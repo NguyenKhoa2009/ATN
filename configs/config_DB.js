b66d6130d157cbce29e6937a40b70a1666137b73
NguyenKhoa2009 <76419303+NguyenKhoa2009@users.noreply.github.com>
2020-12-23 02:00:14 +0700

    Update config_DB.js

--- configs/config_DB.js
+++ configs/config_DB.js
@@ -1,5 +1,6 @@
 module.exports = {
     clouddb: {
+                urldb : 'mongodb+srv://ATN:RJ4jCL3VyVONdXiu@cluster0.jw9uj.mongodb.net/shopsmall?retryWrites=true&w=majority',
         dbname : "shopsmall",
         dbusername : "",
         dbpassword : "",
