14e81c42725f886dcf9a7cd640b0ad0ee8d2bff2
NguyenKhoa2009 <76419303+NguyenKhoa2009@users.noreply.github.com>
2020-12-23 01:53:16 +0700

    Revert "Revert "Update config_DB.js""
    This reverts commit 679f533382917afefa5ad4ef7b0afd21ba814bfa.

--- configs/config_DB.js
+++ configs/config_DB.js
@@ -1,6 +1,6 @@
 module.exports = {
     clouddb: {
-        urldb : 'mongodb+srv://trungshin_6969:QK3xrnkUn8MUUBUw@cluster0.krj6n.mongodb.net/shopsmall?retryWrites=true&w=majority',
+        urldb : 'mongodb+srv://ATN:RJ4jCL3VyVONdXiu@cluster0.jw9uj.mongodb.net/shopsmall?retryWrites=true&w=majority',
         dbname : "shopsmall",
         dbusername : "",
         dbpassword : "",
